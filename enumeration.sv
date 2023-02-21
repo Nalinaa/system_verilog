3d94df34ae9ffa5dd2f6680de2258a25b8866658
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-02-21 15:13:11 +0530

    Create enumeration.sv

---
+++ enumeration.sv
@@ -0,0 +1,9 @@
+module enumeration;
+  typedef enum{TRUE,FALSE}e_true_false;
+  initial
+     begin
+       e_true_false answer;
+       answer=TRUE;
+       $display("answer=%s",answer.name);
+     end
+endmodule
