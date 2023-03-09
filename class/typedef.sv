b5e26417477f7c0d7088e3cba96297628447e182
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-03-09 12:52:40 +0530

    Create typedef.sv

---
+++ class/typedef.sv
@@ -0,0 +1,14 @@
+typedef class nathiya;
+  class nalinaa;
+    nathiya y;
+  endclass
+  class nathiya;
+    nalinaa l;
+  endclass
+  module typedef_class;
+    initial
+      begin
+      
+    $display("inside module");
+    end
+  endmodule
