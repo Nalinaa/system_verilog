0f842513046dbb6a3535b72c534acc7f13f0c962
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-03-09 09:26:37 +0530

    Create protected member.sv

---
+++ class/protected member.sv
@@ -0,0 +1,28 @@
+class parent_class;
+  protected bit[31:0] tmp_addr;
+  function new(bit[31:0] r_addr);
+               tmp_addr=r_addr+10;
+  endfunction
+  function display();
+    $display("tmp_addr=%d",tmp_addr);
+  endfunction
+endclass
+class child_class extends parent_class;
+  function new(bit [31:0] r_addr);
+    super.new(r_addr);
+  endfunction
+  function void incr_addr();
+    tmp_addr++;
+  endfunction
+endclass
+module encapsulation;
+  initial
+    begin
+  parent_class p_c=new(5);
+  child_class c_c=new(10);
+  p_c.tmp_addr=10;
+  p_c.display();
+  c_c.incr_addr();
+  c_c.display();
+  end
+endmodule
