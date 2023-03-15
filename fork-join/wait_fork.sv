8c97fa4e3585e24ccd29ce0b65f3a6321de4f677
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-03-15 07:16:16 +0530

    Create  wait_fork.sv

---
+++ fork-join/wait_fork.sv
@@ -0,0 +1,20 @@
+module wait_fork;
+  initial
+    begin
+      fork
+        begin
+      $display($time,"Process-1 started");
+      #5;
+      $display($time,"process-1 finished");
+    end
+  begin
+    $display($time,"process-2 started");
+    #20;
+    $display($time,"process-2 finished");
+  end
+      join_any
+      wait fork;
+  $display($time,"outside fork-join");
+      $finish;
+  end
+endmodule
